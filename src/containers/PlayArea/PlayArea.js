e62c4792130f6b7967a2d7455d3d8ed6e4e1a467
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-05 05:16:53 +0000

    Initial Commit 2

--- src/containers/PlayArea/PlayArea.js
+++ src/containers/PlayArea/PlayArea.js
@@ -5,25 +5,36 @@ import Contender from '../../components/Contender/Contender';
 const PlayArea = (props) => {
 	return (
 		<div>
-            {
-            props.isPlaying && 
-            <div>
-                <Contender cardDisplay="Dealer Hand:"
-                    cardTotal={props.dealer.count}
-                    cards={props.dealer.cards} />
-                <Contender cardDisplay="Your Hand:"
-                    cardTotal={props.player.count}
-                    cards={props.player.cards} />
-            </div>
-            }
+			{props.isPlaying && (
+				<div>
+					<div className="row">
+						<div className="col-6">
+							<Contender
+								cardDisplay="Your Hand:"
+								cardTotal={props.player.count}
+								cards={props.player.cards}
+							/>
+						</div>
+						<div className="col-6">
+                            This is not working, currently in progress
+							<Contender
+								cardDisplay="Computer Hand:"
+								cardTotal={props.player.count}
+								cards={props.player.cards}
+							/>
+						</div>
+					</div>
+					<Contender cardDisplay="Dealer Hand:" cardTotal={props.dealer.count} cards={props.dealer.cards} />
+				</div>
+			)}
 		</div>
 	);
 };
 
 const mapStateToProps = (state) => ({
-        isPlaying: state.game.isPlaying,
-        dealer: state.game.dealer,
-        player: state.game.player
+	isPlaying: state.game.isPlaying,
+	dealer: state.game.dealer,
+	player: state.game.player
 });
 
 export default connect(mapStateToProps)(PlayArea);
