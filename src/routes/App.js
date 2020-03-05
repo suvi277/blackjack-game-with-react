fa10d05e93d480102997e5e50fef178f54c8c5d6
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-05 05:31:14 +0000

    Added HasRouter to work on gh-pages

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import { HashRouter } from "react-router-dom";
 import Home from './Home';
 import Game from './Game';
 
 const App = () => (
-    <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/game" component={Game} />
-    </Switch>
+    <HashRouter basename='/'>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/game" component={Game} />
+        </Switch>
+    </HashRouter>
 )
 
 export default App;
