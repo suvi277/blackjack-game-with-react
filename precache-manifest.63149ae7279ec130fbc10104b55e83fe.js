bb9be2629a0786ddda466aea477de3a5445999f8
Suvarna Mondal <suvarnamondal27@gmail.com>
2020-03-05 05:23:53 +0000

    Updates

---
+++ precache-manifest.63149ae7279ec130fbc10104b55e83fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd753abcb51a8e8a7e4675cb90b548c9",
+    "url": "/blackjack-game-with-react/index.html"
+  },
+  {
+    "revision": "a465e744217ff54efa62",
+    "url": "/blackjack-game-with-react/static/css/main.d2b555db.chunk.css"
+  },
+  {
+    "revision": "8dc5ddbbc04d5933abf1",
+    "url": "/blackjack-game-with-react/static/js/2.44ea0b1e.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/blackjack-game-with-react/static/js/2.44ea0b1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a465e744217ff54efa62",
+    "url": "/blackjack-game-with-react/static/js/main.bae2acc4.chunk.js"
+  },
+  {
+    "revision": "151c0222ab25ed5cf43a",
+    "url": "/blackjack-game-with-react/static/js/runtime-main.83192342.js"
+  }
+]);
